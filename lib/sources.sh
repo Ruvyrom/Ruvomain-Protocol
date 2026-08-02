e60263d05a74ab5e99504fd11ba7d446c4cb490f
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-02 04:40:06 +0200

    Create sources.sh

---
+++ lib/sources.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+sources() {
+# --- Dynamic Path Resolution ---
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+STYLE_DIR="$REPO_DIR/lib/styles.sh"
+ENSURE_DIR="$REPO_DIR/lib/ensure-adb.sh"
+LOGS_DIR="$REPO_DIR/lib/logs.sh"
+LOGO_DIR="$REPO_DIR/lib/logo.sh"
+INJECTION_DIR="$REPO_DIR/lib/injection.sh"
+VISITOR_DIR="$REPO_DIR/lib/visitor.sh"
+MODEL_DIR="$REPO_DIR/lib/model.sh"
+ENV_DIR="$REPO_DIR/lib/env.sh"
+
+# --- Sources ---
+chmod +x "$STYLE_DIR"
+chmod +x "$ENSURE_DIR"
+chmod +x "$LOGS_DIR"
+chmod +x "$LOGO_DIR"
+chmod +x "$INJECTION_DIR"
+chmod +x "$VISITOR_DIR"
+chmod +x "$MODEL_DIR"
+chmod +x "$ENV_DIR"
+source "$STYLE_DIR"
+source "$ENSURE_DIR"
+source "$LOGS_DIR"
+source "$LOGO_DIR"
+source "$INJECTION_DIR"
+source "$VISITOR_DIR"
+source "$MODEL_DIR"
+source "$ENV_DIR"
+} 
