11973454ddcc0a986520b55aa81beaf366002882
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-02 04:10:38 +0200

    Create model.sh

---
+++ lib/model.sh
@@ -0,0 +1,5 @@
+printf "${CYAN}${BOLD}Ruvomain-PBD | Pure Bash Debloater${NC}\n"
+printf "%s\n" "${CYAN}------------------------------------------${NC}\n"
+CURRENT_MODEL=$(getprop ro.product.model 2>/dev/null || adb shell getprop ro.product.model 2>/dev/null || echo "Unknown")
+printf "${GREEN}Device detected:${NC}\n ${BOLD}%s${NC}\n" "${CURRENT_MODEL}"
+printf "%s\n" "${CYAN}------------------------------------------${NC}\n"
