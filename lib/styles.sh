859f4c03dd491fa42113ac7a6b7c7d21cfe07b0a
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-02 02:57:08 +0200

    Update styles.sh

--- lib/styles.sh
+++ lib/styles.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 BLUE='\033[0;34m'
 BOLD='\033[1m'
 CYAN='\033[0;36m'
@@ -6,3 +8,7 @@ RED='\033[1;31m'
 WHITE='\033[0;37m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
+
+log_info() { echo-e "${CYAN}[INFO]${NC} $1"; }
+log_success() { echo -e "${GREEN}[SUCCESS]${NC} $1"; }
+log_error() { echo -e "${RED}[ERROR]${NC} $1"; }
