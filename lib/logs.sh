35a7b528455ee351e42ecd1a8cccf1ef542da90e
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-05 17:02:47 +0200

    Update logs.sh

--- lib/logs.sh
+++ lib/logs.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+"$REPO_DIR/logs"="$1";
+
 init_logs() {
-mkdir -p "$REPO_DIR/logs"
+mkdir -p "$1"
 
-find "$REPO_DIR/logs" -name "ruvomain-*.log" -type f -mtime +30 -delete 2>/dev/null
+find "$1" -name "ruvomain-*.log" -type f -mtime +30 -delete 2>/dev/null
 
-LOGFILE="$REPO_DIR/logs/ruvomain-$(date +%Y%m%d_%H%M%S).log"
+LOGFILE="$1"/ruvomain-$(date +%Y%m%d_%H%M%S).log"
 
 exec > >(tee -a "$LOGFILE") 2>&1
 }
