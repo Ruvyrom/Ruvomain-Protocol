6c0453df1e26ef18108e834e61d0098d433c0aff
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-13 14:19:12 +0200

    Update ruvomain.sh

--- ruvomain-pbd/ruvomain.sh
+++ ruvomain-pbd/ruvomain.sh
@@ -5,6 +5,8 @@
 set -euo pipefail
 
 # --- Dynamic Path Resolution and sources---
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 SOURCES_DIR="$REPO_DIR/lib/sources.sh"
 chmod +x "$SOURCES_DIR"
 source "$SOURCES_DIR"
