eecdbbfc967800a7ce6b8783a9676150d5791f82
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-29 16:58:34 +0200

    Delete lib/env.sh

--- lib/env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-env() {
-if [ -d "/data/data/com.termux" ] || [ -f "/system/bin/pm" ]; then
-    EXEC="pm uninstall -k --user 0"
-else
-    EXEC="adb shell pm uninstall -k --user 0"
-fi
-} 
