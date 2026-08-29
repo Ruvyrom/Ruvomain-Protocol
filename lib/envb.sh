1e1d5c5faf2a351642d62bc9799f2d75c1c72cae
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-29 16:58:53 +0200

    Delete lib/envb.sh

--- lib/envb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-envb() {
-if [ -d "/data/data/com.termux" ] || [ -f "/system/bin/pm" ]; then
-    EXEC="pm install-existing --user0"
-else
-    EXEC="adb shell pm install-existing --user0"
-fi
-} 
