01c7e31e31448b470b81d8fb722cd34f071f0a66
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-08-29 15:48:24 +0200

    Create env-detect.sh

---
+++ lib/env-detect.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+env_detect() {
+    if [ -d "/data/data/com.termux" ]; then
+    EXEC=""
+    echo -e "${BLUE}[Local Termux Mode detected]${NC}"
+    else
+    EXEC="adb shell"
+    echo-e "${BLUE}[Remote Linux/ADB Mode detected]${NC}"
+    fi
+} 
