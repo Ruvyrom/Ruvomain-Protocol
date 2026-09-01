7ebcb23bcf01328854703de54e48cfb96224c677
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 15:58:24 +0200

    Create vl-menu.sh

---
+++ lib/vl-menu.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+vl_menu() {
+clear
+echo "Select log type you want see"
+options=(
+"Debloat Logs"
+"Restore Logs"
+"Backup Logs"
+"Return to Dashboard"
+"Exit"
+)
+
+PS3="Select a module to execute (1-5): "
+select opt in "${options[@]}"; do
+case $opt in
+"Debloat Logs")
+clear
+view_dlogs
+;;
+"Restore Logs")
+clear
+view_rlogs
+;;
+"Backup Logs")
+clear
+view_blogs
+;;
+"Return to Dashboard")
+clear
+menu
+;;
+"Exit")
+echo "Exiting protocol..."
+exit 0
+;;
+*) echo -e "${RED}Invalid option. Please choose 1-6.${NC}";;
+esac
+done
+}
