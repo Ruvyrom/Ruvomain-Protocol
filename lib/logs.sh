4a5b7291c062676fa900dbae6df676eee74d0cf1
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 14:31:20 +0200

    Update logs.sh

--- lib/logs.sh
+++ lib/logs.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-"$REPO_DIR/ruvomain-debloat/logs"="$1";
-
 init_logs() {
-mkdir -p "$1"
+mkdir -p $REPO_DIR/ruvomain-debloat/logs
 
-find "$1" -name "ruvomain-debloat-*.log" -type f -mtime +30 -delete 2>/dev/null
+find $REPO_DIR/ruvomain-debloat/logs -name "ruvomain-debloat-*.log" -type f -mtime +30 -delete 2>/dev/null
 
-LOGFILE="$1"/ruvomain-debloat-$(date +%Y%m%d_%H%M%S).log"
+LOGFILE=$REPO_DIR/ruvomain-debloat/logs/ruvomain-debloat-$(date +%Y%m%d_%H%M%S).log"
 
 exec > >(tee -a "$LOGFILE") 2>&1
 }
