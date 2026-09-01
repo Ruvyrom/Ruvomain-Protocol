06a0c693fe4ac2cbfd5efef665abd8ee2c6b314b
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 02:24:47 +0200

    Update ruvomain-restore.sh

--- ruvomain-restore/ruvomain-restore.sh
+++ ruvomain-restore/ruvomain-restore.sh
@@ -2,7 +2,7 @@
 # Ruvomain ADB-Termux Restorer script ( Bash/JQ )
 # You can apply your own json debloat list file. Use or place your *.json file in ./ruvomain-backup/backups and select it in menu.
 # Version 2.0.0 (Refactored for Ruvomain Protocol - Surgical Minimalism)
-# Created by Ruvyrom
+# Created by Ruvyrom (https://github.com/Ruvyrom/Ruvomain-Protocol)
 set -euo pipefail
 
 # --- Dynamic Path Resolution and sources---
