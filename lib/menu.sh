ffcbc3403cbefbe962000cdf611726b1a17a36f8
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 13:58:06 +0200

    Update menu.sh

--- lib/menu.sh
+++ lib/menu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 menu() {
+clear
 options=(
 "Termux Setup (Semi-auto Termux setup)"
 "Installer (APK installation)"
@@ -14,19 +15,19 @@ PS3="Select a module to execute (1-6): "
 select opt in "${options[@]}"; do
 case $opt in
 "Termux Setup (Setup Termux before scripts exexution)")
-"$t"
+$REPO_DIR/termux-setup.sh
 ;;
-"Installer (Manage Initial Setup)")
-"$r2""$i"/"$r1""$i".sh
+"Installer (APK installation)")
+$REPO_DIR/ruvomain-installer/ruvomain-installer.sh
 ;;
 "Debloat (System Optimization)")
-"$r2""$d"/"$r1""$d".sh
+$REPO_DIR/ruvomain-debloat/ruvomain-debloat.sh
 ;;
 "Backup (Snapshot Data)")
-"$r2""$b"/"$r1""$b".sh
+$REPO_DIR/ruvomain-backup/ruvomain-backup.sh
 ;;
 "Restore (Revert/Reinstall Apps)")
-$r2""$r3"/"$r1""$r3".sh
+$REPO_DIR/ruvomain-restore/ruvomain-restore.sh
 ;;
 "Exit")
 echo "Exiting protocol..."
