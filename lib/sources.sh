4a1df92cbc70351464b8ebbda5ee7c0d1dbab59f
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 12:20:22 +0200

    Update sources.sh

--- lib/sources.sh
+++ lib/sources.sh
@@ -2,20 +2,21 @@
 
 sources() {
 #Variable
-"android-tools"="$1"; 
-"adb"="$2";
-"sudo"="$3";
-"ruvomain-"="$r1";
-"./ruvomain-"="$r2";
-"./termux-setup.sh"="$t";
-"installer"="$i";
-"debloat"="$d";
-"backup"="$b";
-"restore"="$r3";
-"$REPO_DIR/ruvomain-backup/logs"="$bl";
+android-tools="$1"; 
+adb="$2";
+sudo="$3";
+ruvomain-="$r1";
+./ruvomain-="$r2";
+./termux-setup.sh="$t";
+installer="$i";
+debloat="$d";
+backup="$b";
+restore="$r3";
+$REPO_DIR/ruvomain-backup/logs="$bl";
+$REPO_DIR/lib/="$rdl";
 
 # --- Dynamic Path Resolution ---
-STYLE_DIR="$REPO_DIR/lib/styles.sh"
+STYLE_DIR="$"$rdl"styles.sh"
 ENSURE_DIR="$REPO_DIR/lib/ensure-adb.sh"
 ENSUREJQ_DIR="$REPO_DIR/lib/ensure-jq.sh"
 LOGS_DIR="$REPO_DIR/lib/logs.sh"
@@ -23,17 +24,17 @@ LOGO_DIR="$REPO_DIR/lib/logo.sh"
 MODEL_DIR="$REPO_DIR/lib/model.sh"
 ENV_DIR="$REPO_DIR/lib/env-detect.sh"
 MENU_DIR="$REPO_DIR/lib/menu.sh"
-CONFIGS_DIR="$REPO_DIR/lib/logs-backup.sh"
+CONFIGS_DIR="$REPO_DIR/lib/logs-"$b".sh"
 BACKUPS_DIR="$REPO_DIR/Configs"
 FINAL_DIR="$REPO_DIR/lib/final.sh"
 CHECK_DIR="$REPO_DIR/lib/check_adb.sh"
-INSTALLER_DIR="$REPO_DIR/lib/installer.sh"
-BACKUP_DIR="$REPO_DIR/lib/backup.sh"
-LOGSBACKUP_DIR="$REPO_DIR/lib/logs-backup.sh"
-LOGSRESTORE_DIR="$REPO_DIR/lib/logs-restore.sh"
-BACKUPS_DIR="$(dirname "$0")/../ruvomain-backup/backups"
-DEBLOAT_DIR="$REPO_DIR/lib/debloat.sh"
-RESTORE_DIR="$REPO_DIR/lib/restore.sh"
+INSTALLER_DIR="$REPO_DIR/lib/"$i".sh"
+BACKUP_DIR="$REPO_DIR/lib/"$b".sh"
+LOGSBACKUP_DIR="$REPO_DIR/lib/logs-"$b".sh"
+LOGSRESTORE_DIR="$REPO_DIR/lib/logs-"$r3".sh"
+BACKUPS_DIR="$(dirname "$0")/."$r2""$b"/"$b"s"
+DEBLOAT_DIR="$REPO_DIR/lib/"$d".sh"
+RESTORE_DIR="$REPO_DIR/lib/"$r3".sh"
 AUTOP_DIR="$REPO_DIR/lib/autoperm.sh"
 
 # --- Sources & Execution Permissions ---
