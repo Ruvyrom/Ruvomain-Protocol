bccb94f52b36e63749124253f480119e02b0efbc
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 10:57:03 +0200

    Update menu.sh

--- lib/menu.sh
+++ lib/menu.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-display_main_menu() {
-    local import_dir="$REPO_DIR/Configs/Imports"
-    if [ ! -d "$import_dir" ]; then
-        printf "${RED}[!] Folder not found: %s${NC}\n" "$import_dir" >&2
-        return 1
-    fi
-    local files=("$import_dir"/*.json)
-    if [ ! -e "${files[0]}" ]; then
-        printf "${RED}[!] No configs found in %s\n${NC}" "$import_dir" >&2
-        return 1
-    fi
+menu() {
+options=(
+"Termux Setup (Semi-auto Termux setup)"
+"Installer (APK installation)"
+"Debloat (System Optimization)"
+"Backup (Snapshot Data)"
+"Restore (Revert/Install Apps)"
+"Exit"
+)
 
-    select opt in "${files[@]}" "Return to main menu"; do
-        if [[ "$opt" == "Return to main menu" ]]; then return 1; fi
-        if [ -f "$opt" ]; then
-            JSON_FILE="$opt"
-            printf "%s\n" "--- Configuration Details ---"
-            printf "Name: %s\n" "$(get_json_val "$opt" "name")"
-            printf "Version: %s\n" "$(get_json_val "$opt" "version")"
-            read -r -p "Apply? (y/N): " confirm
-            [[ "$confirm" == [yY] ]] && return 0 || return 1
-        fi
-    done
-}
+PS3="Select a module to execute (1-6): "
+select opt in "${options[@]}"; do
+case $opt in
+"Termux Setup (Setup Termux before scripts exexution)")
+"$t"
+;;
+"Installer (Manage Initial Setup)")
+"$r2""$i"/"$r1""$i".sh
+;;
+"Debloat (System Optimization)")
+"$r2""$d"/"$r1""$d".sh
+;;
+"Backup (Snapshot Data)")
+"$r2""$b"/"$r1""$b".sh
+;;
+"Restore (Revert/Reinstall Apps)")
+$r2""$r3"/"$r1""$r3".sh
+;;
+"Exit")
+echo "Exiting protocol..."
+exit 0
+;;
+*) echo -e "${RED}Invalid option. Please choose 1-6.${NC}";;
+esac
+done
