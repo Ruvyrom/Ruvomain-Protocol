45a9edadca3eb3a0bc92b856f19fb3373e44ec42
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-02 00:44:43 +0200

    Update vl-menu.sh

--- lib/vl-menu.sh
+++ lib/vl-menu.sh
@@ -18,7 +18,6 @@ echo "--- Santé du Système ---"
 adbshell dumpsys battery | grep "level"
 adb shell dumpsys cpuinfo | head -n 1
 
-echo "Select log type you want see"
 options=(
 "Debloat Logs"
 "Restore Logs"
