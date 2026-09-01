30849fe891325f7db2d1dd8c64ecfc49cf660d18
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 16:19:42 +0200

    Update vl-menu.sh

--- lib/vl-menu.sh
+++ lib/vl-menu.sh
@@ -2,14 +2,15 @@
 
 REPO_DIR="$(dirname "$(dirname "$(readlink -f"$0")")")"
 SOURCES_DIR="$REPO_DIR/lib/sources.sh"
-if [ -f "$SOURCES_DIR" ]; then
+if [ -z "$SOURCES_LOADED" ]; then
 chmod +x "$SOURCES_DIR"
 source "$SOURCES_DIR"
 else
 echo "Error: Could not find $SOURCES_DIR"
 exit 1
-fi
 sources
+export SOURCES_LOADED=1
+fi
 
 vl_menu() {
 clear
