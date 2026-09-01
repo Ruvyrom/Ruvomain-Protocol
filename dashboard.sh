e8ad7e81639ae0e20a88c675e0eb8c196b7da328
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-01 10:31:22 +0200

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -1,29 +1,42 @@
 #!/usr/bin/env bash
-# Ruvomain-Protocol Control Center
+# Universal Ruvomain ADB Apps-Manager (URAAM) - Control Center
 # Surgical access to your protocol modules
 # Version: v2.0.0
 
-# Colors
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-NC='\033[0m'
+# --- Dynamic Path Resolution and sources---
+REPO_DIR="$(cd "./Ruvomain-Protocole" && pwd)"
+SOURCES_DIR="$REPO_DIR/lib/sources.sh"
+if [ -f "$SOURCES_DIR" ]; then
+chmod +x "$SOURCES_DIR"
+source "$SOURCES_DIR"
+else
+echo "Error: Could not find $SOURCES_DIR"
+exit 1
+fi
+sources
+
+"ruvomain-"="$r1";
+"./ruvomain-"="$r2";
+"./termux-setup.sh"="$t";
+"installer"="$i";
+"debloat"="$d";
+"backup"="$b";
+"restore"="$r3";
 
 clear
 echo -e "${BLUE}=========================================="
 echo -e "   RUVOMAIN-PROTOCOL | CONTROL CENTER"
 echo -e "==========================================${NC}"
 show_logo
+
 # --- Auto-fix Permissions (Targeted) ---
-modules=("ruvomain-installer" "ruvomain-debloat" "ruvomain-backup" "ruvomain-restore")
+modules=(""$t" "$r2""$i"/"$r1""$i".sh" "$r2""$d"/"$r1""$d".sh" ""$r2""$b"/"$r1""$b".sh" ""$r2""$r3"/"$r1""$r3".sh")
 
 for mod in "${modules[@]}"; do
-if [ -d "$mod" ]; then
-chmod +x "$mod"/*.sh
+if [ -f "$mod" ]; then
+chmod +x "$mod"
 fi
 done
-
-chmod +x ./termux-setup.sh
 
 # Define the options
 options=(
@@ -39,24 +52,19 @@ PS3="Select a module to execute (1-6): "
 select opt in "${options[@]}"; do
 case $opt in
 "Termux Setup (Setup Termux before scripts exexution)")
-./termux-setup.sh
-break
+"$t"
 ;;
 "Installer (Manage Initial Setup)")
-./ruvomain-installer/ruvomain-installer.sh
-break
+"$r2""$i"/"$r1""$i".sh
 ;;
 "Debloat (System Optimization)")
-./ruvomain-debloat/ruvomain-debloat.sh
-break
+"$r2""$d"/"$r1""$d".sh
 ;;
 "Backup (Snapshot Data)")
-./ruvomain-backup/ruvomain-backup.sh
-break
+"$r2""$b"/"$r1""$b".sh
 ;;
 "Restore (Revert/Reinstall Apps)")
-./ruvomain-restore/ruvomain-restore.sh
-break
+$r2""$r3"/"$r1""$r3".sh
 ;;
 "Exit")
 echo "Exiting protocol..."
