64d3bcfa09d703b9d17dfb0db226f6f1ce8cf52e
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-04 13:31:02 +0200

    Update ruvomain-debloat.sh

--- ruvomain-debloat/ruvomain-debloat.sh
+++ ruvomain-debloat/ruvomain-debloat.sh
@@ -11,11 +11,10 @@ SOURCES_DIR="$REPO_DIR/lib/sources.sh"
 if [ -f "$SOURCES_DIR" ]; then
 chmod +x "$SOURCES_DIR"
 source "$SOURCES_DIR"
+sources
 else
 echo "Error: Could not find $SOURCES_DIR"
 exit 1
-sources
-export SOURCES_LOADED=1
 fi
 
 show_logo
