d62baccc2f0bf110088258fd29af5cb3532b2f00
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-04 13:50:29 +0200

    Update model.sh

--- lib/model.sh
+++ lib/model.sh
@@ -9,9 +9,5 @@ printf "%s\n" "${CYAN}------------------------------------------${NC}\n"
 } 
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-model()
-fi
-
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 model
 fi
