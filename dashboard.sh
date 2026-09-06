c9d00a0b8cb35fc84279408cf1ba726fcb3d31f9
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-06 17:30:56 +0200

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -19,16 +19,27 @@ fi
 clear
 show_logo
 echo -e "${BLUE}=========================================="
-echo -e " URAAM RUVOMAIN ADB APP-MANAGER | CONTROL CENTER"
+echo -e "URAAM RUVOMAIN ADB APP-MANAGER | CONTROL CENTER"
 echo -e "==========================================${NC}"
+
+termux_setup(){
+if [ -d "/data/data/com.termux" ] && command -v termux-setup-storage >/dev/null 2>&1; then
+termux-setup-storage
+fi
+} 
+
 echo -e "before use a script:"
 echo -e "For Ruvomain-debloat, place your personal or Canta JSON lists in ./Configs"
 echo -e "For Ruvomain-installer, place your APK files in ./ruvomain-installer/Apps"
 echo -e "For Ruvomain-restore, use your backup created with ruvomain-backup.sh or place your own backup .json file or Canta .json file list in ./ruvomain-backup/backups"
 echo -e "Ruvomain-backup places your backup .json file in /ruvomain-backup/backups"
-
+echo "[*] Requesting storage access (please confirm the popup)..."
 
 ensure-adb || exit 1
 ensure-jq || exit 1
 autoperm
 menu
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+termux_setup
+fi
