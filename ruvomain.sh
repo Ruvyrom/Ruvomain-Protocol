0f407d10718fca65f932ff0d137454770ad2c0de
Ruvomain <112347754+Ruvyrom@users.noreply.github.com>
2026-09-09 00:30:10 +0200

    Update ruvomain.sh

--- ruvomain.sh
+++ ruvomain.sh
@@ -103,7 +103,7 @@ y|Y)
 printf "${GREEN}[+] Attempting automatic installation...${NC}\n"
 # 3. Existing installation logic
 if command -v pkg >/dev/null; then
-pkg install -y adb
+pkg install -y android-tools
 elif command -v apt-get >/dev/null; then
 sudo apt-get update && sudo apt-get install -y adb
 elif command -v pacman >/dev/null; then
