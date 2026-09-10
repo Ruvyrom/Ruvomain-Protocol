ab55c914c7d34382d08b25c21eedfdb4640559e8
URAAM <112347754+Ruvyrom@users.noreply.github.com>
2026-09-11 01:20:02 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -21,8 +21,7 @@ NC='\033[0m'
 
 cleanup() {
 printf "${CYAN}[*] Cleaning up...${NC}\n"
-rm -rf "$INSTALL_DIR/assets"
-rm -f "$0" 2>/dev/null || true
+rm -rf "$INSTALL_DIR/assets" "$INSTALL_DIR/installer.sh"
 }
 
 target() {
