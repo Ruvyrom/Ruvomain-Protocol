6dfdb4eb516335fb85da019c2eb32d64acc5d8ad
URAAM <112347754+Ruvyrom@users.noreply.github.com>
2026-09-12 03:34:14 +0200

    Update uraam.sh

--- uraam.sh
+++ uraam.sh
@@ -792,7 +792,7 @@ if [ -f "$INSTALL_DIR/uraam.sh" ]; then
 chmod +x "$INSTALL_DIR/uraam.sh"
 find "$INSTALL_DIR" -type f -name "*.sh" -exec chmod +x {} +
 sleep 1 
-read -rp "press Enter to restarting URAAM with new changes..." 
+read -rp "press Enter to restart URAAM with new changes..." 
 rm -rf "$INSTALL_DIR/assets" "$INSTALL_DIR/installer.sh"
 clear
 exec "$0" "$@"
